ff41c3d1d7172b0a9af5b62f7c132e1ac3909244
readnthink <sol4854@naver.com>
2025-01-22 23:25:17 +0900

    refactor : 중간 데이터 객체 StatementData 생성 및 renderPlanText함수 적용

---
+++ src/main/java/org/example/refactoring/ch01/StatementData.java
@@ -0,0 +1,15 @@
+package org.example.refactoring.ch01;
+
+public class StatementData {
+    private Invoice invoice;
+    private Plays plays;
+
+    public StatementData(Invoice invoice, Plays plays) {
+        this.invoice = invoice;
+        this.plays = plays;
+    }
+
+    public String getCustomer() {
+        return invoice.getCustomer(); 
+    }
+}

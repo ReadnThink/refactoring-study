9ffad162e3919554d4054ae923d5f5917ba9a6bf
readnthink <sol4854@naver.com>
2025-01-23 00:09:41 +0900

    feat : renderHtml 메서드, 테스트 추가

--- src/main/java/org/example/refactoring/ch01/Statement.java
+++ src/main/java/org/example/refactoring/ch01/Statement.java
@@ -3,7 +3,7 @@ package org.example.refactoring.ch01;
 public class Statement {
     public String statement(Invoice invoice, Plays plays) throws Exception {
         StatementData statementData = new StatementData(invoice, plays);
-        return renderPlanText(statementData, plays);
+        return renderHtml(statementData, plays);
     }
 
     private String renderPlanText(StatementData statementData, Plays plays) throws Exception {
@@ -15,6 +15,20 @@ public class Statement {
 
         result.append(String.format("총액: $%d\n", statementData.getTotalAmount(statementData.getInvoice(), plays) / 100));
         result.append(String.format("적립 포인트: %d점\n", statementData.totalVolumeCredits(statementData.getInvoice(), plays)));
+        return result.toString();
+    }
+
+    private String renderHtml(StatementData statementData, Plays plays) throws Exception {
+        StringBuilder result = new StringBuilder(String.format("<h1>청구 내역 (고객명: %s)</h1>\n", statementData.getCustomer()));
+        result.append("<table>\n");
+        result.append("<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n");
+        for (Performance performances : statementData.getPerformances()) {
+            result.append(String.format("<tr><td>%s</td><td>%d</td><td>%d</td></tr>\n",statementData.playFor(plays, performances).getName(), performances.getAudience(), statementData.amountFor(performances, plays) / 100));
+        }
+        result.append("</table>\n");
+
+        result.append(String.format("<p>총액: <em>%d</em></p>\n", statementData.getTotalAmount(statementData.getInvoice(), plays) / 100));
+        result.append(String.format("<p>적립 포인트: <em>%d</em>점</p>", statementData.totalVolumeCredits(statementData.getInvoice(), plays)));
         return result.toString();
     }
 }
--- src/test/java/org/example/refactoring/ch01/StatementTest.java
+++ src/test/java/org/example/refactoring/ch01/StatementTest.java
@@ -40,4 +40,38 @@ class StatementTest {
         // then
         assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    void htmlTest() throws Exception {
+        // given
+        Map<String, Play> playMap = new HashMap<>();
+        playMap.put("hamlet", new Play("Hamlet", PlayType.TRAGEDY));
+        playMap.put("as-like", new Play("As You Like It", PlayType.COMEDY));
+        playMap.put("othello", new Play("Othello", PlayType.TRAGEDY));
+        Plays plays = new Plays(playMap);
+
+        ArrayList<Performance> performances = new ArrayList<>();
+        performances.add(new Performance("hamlet", 55));
+        performances.add(new Performance("as-like", 35));
+        performances.add(new Performance("othello", 40));
+        Invoice invoice = new Invoice("BigCo", performances);
+        String expected = """
+                <h1>청구 내역 (고객명: BigCo)</h1>
+                <table>
+                <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>
+                <tr><td>Hamlet</td><td>55</td><td>650</td></tr>
+                <tr><td>As You Like It</td><td>35</td><td>580</td></tr>
+                <tr><td>Othello</td><td>40</td><td>500</td></tr>
+                </table>
+                <p>총액: <em>1730</em></p>
+                <p>적립 포인트: <em>47</em>점</p>""";
+        Statement statement = new Statement();
+
+        // when
+        String actual = statement.statement(invoice, plays);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
 }
